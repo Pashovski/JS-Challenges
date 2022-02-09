125f4afbf0c9598e7be96001c00b9cea0ca4f188
Pasha <pashdoctor@gmail.com>
2022-02-09 10:11:49 -0500

    this a new commit

---
+++ index.js
@@ -0,0 +1,16 @@
+//CHALLENGE 1: REVERSE A STRING
+// Return a stirng in reverse
+// ex. reverseString('hello') === 'olleh'
+
+function reverseString(str) {
+    return 'Let\'s Start'
+}
+
+//CHALLENGE 2: VALIDATE A PALINDROME
+//Return true if palindrome and false if not
+// ex. isPalindrome('racecare') === 'true', isPalindrome('hello')
+
+//Call Function
+const output = reverseString('hello');
+
+console.log(output);
